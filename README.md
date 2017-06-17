34be23fea0c1f662c8b87afc6e55e9c626d312ba
mk-pmb <mk-pmb@users.noreply.github.com>
2017-06-17 02:17:30 +0200

    v0.1.0

---
+++ README.md
@@ -0,0 +1,34 @@
+﻿
+<!--#echo json="package.json" key="name" underline="=" -->
+async-util-pmb
+==============
+<!--/#echo -->
+
+<!--#echo json="package.json" key="description" -->
+Helper functions for preparing task queues for the `async` module.
+<!--/#echo -->
+
+
+Docs = annotations in [the source](au.js).
+
+
+<!--#toc stop="scan" -->
+
+
+
+Known issues
+------------
+
+* needs more/better tests and docs
+
+
+
+
+&nbsp;
+
+
+License
+-------
+<!--#echo json="package.json" key=".license" -->
+ISC
+<!--/#echo -->
---
+++ au.js
@@ -0,0 +1,50 @@
+﻿/*jslint indent: 2, maxlen: 80, continue: false, unparam: false, node: true */
+/* -*- tab-width: 2 -*- */
+'use strict';
+
+var au = {}, arSlc = Array.prototype.slice;
+
+
+au.wfTap = function (f) {
+  // Interrupt the waterfall for async f, then continue with original args.
+  return function () {
+    var args = arSlc.call(arguments), aThen = args.pop();
+    function fThen(err) { return aThen.apply(null, [err].concat(args)); }
+    return f.apply(null, args.concat(fThen));
+  };
+};
+
+
+au.wfMod = function (f) {
+  // Get a chance to modify the args sync:
+  return function () {
+    var args = arSlc.call(arguments), aThen = args.pop();
+    args = (f.apply(args, args) || args);
+    aThen.apply(null, args);
+  };
+};
+
+
+au.wfDropArgs = function () { return arguments[arguments.length - 1](); };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = au;
